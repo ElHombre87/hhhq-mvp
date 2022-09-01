f598c43538e147657339da5b27a1d4117112bf98
dinghino <dinghino@gmail.com>
2022-09-01 15:02:37 +0200

    add extra global typings
    OptionalPropertiesOf to get all the key optional in an object
    Optionals to get a non-optional object with the optional keys of the given object

--- apps/playground/src/libs/types.ts
+++ apps/playground/src/libs/types.ts
@@ -3,3 +3,19 @@
  */
 
 export type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+/**
+ * Extracts the optional property keys of a given object
+ */
+export type OptionalPropertyOf<T extends object> = Exclude<
+  {
+    [K in keyof T]: T extends Record<K, T[K]> ? never : K;
+  }[keyof T],
+  undefined
+>;
+
+/**
+ * Returns an object-like type with all the optional properties of the given type
+ * requried, with undefined exluded (but not null)
+ */
+export type Optionals<T extends object> = {[key in OptionalPropertyOf<T>]: Exclude<T[key], undefined> }
